2502690767543618245aaa584bbbf8037a4df8b4
b20isasa <b20isasa@student.his.se>
2023-05-19 14:39:04 +0200

    Ändrat tillbaka till JsonTask

---
+++ app/src/main/java/com/example/project/JsonFileData.java
@@ -0,0 +1,5 @@
+package com.example.project;
+
+public class JsonFileData {
+ //   [{"ID":"b20isasa","name":"","type":"b20isasa","company":"","location":"","category":"","size":0,"cost":0,"auxdata":""},{"ID":"jänta","name":"Maja","type":"b20isasa","company":"","location":"Sverige","category":"","size":2,"cost":0,"auxdata":""},{"ID":"killar","name":"Noah","type":"b20isasa","company":"","location":"Sverige","category":"","size":4,"cost":0,"auxdata":""},{"ID":"kille","name":"Liam","type":"b20isasa","company":"","location":"Sverige","category":"","size":4,"cost":0,"auxdata":""},{"ID":"tjej","name":"Alice","type":"b20isasa","company":"","location":"Sverige","category":"","size":2,"cost":0,"auxdata":""},{"ID":"tjejer","name":"Sanna","type":"b20isasa","company":"","location":"Sverige","category":"","size":2,"cost":0,"auxdata":""}}]
+}
--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -18,21 +18,24 @@ import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+@SuppressWarnings("FieldCanBeLocal")
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
     private RecyclerView recyclerView;
     private Button knappen;
     private MyAdapter adapter;
     private ArrayList<Items> ProjectList;
-    private TextView textView;
-    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=b20isasa";
+
+    //private final String JSON_file = ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //new JsonTask(this).execute(JSON_file);
 
-        textView = findViewById(R.id.textView);
 
         adapter = new MyAdapter(ProjectList);
         recyclerView = findViewById(R.id.recyclerView);
@@ -45,13 +48,15 @@ public class MainActivity extends AppCompatActivity {
         knappen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("==>", "tillbaka");
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(intent);
+          Log.d("==>", "tillbaka");
+          Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+          startActivity(intent);
             }
         });
 
-    }public void onPostExecute(String json){
+    }
+
+    public void onPostExecute(String json){
         Gson gson = new Gson();
         Type type = new TypeToken<ArrayList<Items>>(){}.getType();
         ArrayList<Items> Alista = gson.fromJson(json, type);
